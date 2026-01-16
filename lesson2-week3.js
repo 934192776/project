3089b92aeb26879a2acf008bbd9218c0cf3b8658
jinghui <934192776@qq.com>
2026-01-16 15:40:07 +0800

    update

---
+++ lesson2-week3.js
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Greek Yogurt</title>
+</head>
+<body>
+    <h1>
+        I love Yogurt
+        </h1>
+        <h2>
+            Especially Greek Yogurt
+            </h2>
+            <img
+            src="https://media.post.rvohealth.io/wp-content/uploads/2021/10/fruit-berries-yogurt-732x549-thumbnail-732x549.jpg"
+             alt="Greek Yogurt"
+             width="200"
+             />
+             <p>
+                Greek yogurt is made by straining out the extra whey in regular yogurt, which makes it thicker, creamier and tangier than regular yogurt. While plain Greek yogurt has less sugar and more protein than regular yogurt, regular yogurt delivers almost twice the bone-strengthening mineral calcium than Greek yogurt does.
+              <br />
+              <a href="https://www.eatingwell.com/article/7877433/which-is-healthier-greek-yogurt-or-regular-yogurt/"
+              target="-blank">
+              learn more on wikipedia
+              </a>
+            </p>
+            <button>
+               buy Greek Yogurt
+                </button>
+                This page built by
+                <a href="https://www.jhui.io/" target="_blank">
+                Jing Hui
+
+</body>
+</html>
